d7d81dbc7be8851e58449b04106b4c5c59898fae
Shin-Min-Thant <mshin7587@gmail.com>
2023-10-16 17:19:45 +0630

    refresh

--- resources/js/Components/Loader.jsx
+++ resources/js/Components/Loader.jsx
@@ -1,22 +1,15 @@
-import LoadingBar from 'react-top-loading-bar'
-import {useAppStateContext} from '../Context/AppStateContext'
+import LoadingBar from "react-top-loading-bar";
+import { useAppStateContext } from "../Context/AppStateContext";
 // import useLoader from '../Hooks/useLoader'
 
 const Loader = () => {
+    const { loaderRef } = useAppStateContext();
 
-	const {loaderRef} = useAppStateContext();
-
-	return (
-		<div className="fixed top-0 z-50 w-full" id="loader">
-			<LoadingBar
-				color='#04f491' 
-				ref={loaderRef}
-			/>
-		</div>
-	)
-}
-
-
-
+    return (
+        <div className="fixed top-0 z-50 w-full" id="loader">
+            <LoadingBar color="#04f491" ref={loaderRef} />
+        </div>
+    );
+};
 
 export default Loader;
